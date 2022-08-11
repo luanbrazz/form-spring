8954798197e62002784f5b979399c35ebdf01367
Luan Braz <luan_silvacpv@hotmail.com>
2022-08-11 17:44:01 -0300

    spring boot controller

---
+++ src/test/java/com/cursospring/codegate/projetoaula/ProjetoaulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursospring.codegate.projetoaula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoaulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
